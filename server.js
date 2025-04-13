030bb83018eadeb981e74fa95dfad6d5fc22f013
Cha Li <lifuzhang1991@gmail.com>
2025-04-12 23:12:04 -0400

    Fixed api/user/login post method

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/api/user/register", (req, res) => {
     });
 });
 
-app.post("/api/login", (req, res) => {
+app.post("/api/user/login", (req, res) => {
     userService.checkUser(req.body)
         .then((user) => {
 
